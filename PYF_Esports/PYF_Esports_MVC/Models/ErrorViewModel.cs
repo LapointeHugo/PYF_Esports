3c9514b2bda31b21b622da1b533f1ba95c1403eb
Hugo <1946091@cegepmontpetit.ca>
2024-03-05 15:20:33 -0500

    PROGRESS: Creation of project MVC

---
+++ PYF_Esports/PYF_Esports_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PYF_Esports_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
