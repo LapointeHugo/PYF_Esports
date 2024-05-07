0deb8e2ce89ade14a6a2a5d344ec395f262a7ff7
Hugo <1946091@cegepmontpetit.ca>
2024-05-07 16:53:24 -0400

    FCT: Login page done

--- PYF_Esports/views/auth.php
+++ PYF_Esports/views/auth.php
@@ -1,14 +1,19 @@
-<div class="container row">
-    <div class="col-6">
+<div class="container row justify-content-around">
+    <div class="col-5">
         <h2>Admin Login</h2>
-        <p>Welcome to the admin login page. We're currently working on implementing a login
+        <p style="text-align: justify">Welcome to the admin login page. We're currently working on implementing a login
             system for regular users, but it's not available yet. Stay tuned for updates!
             Meanwhile, if you're an admin, please proceed with your credentials.</p>
-        <img src="/images/Cypher.webp" height="200px" width="200px">
+            <br>
+        <div class="row justify-content-around">
+            <img src="/images/Cypher.webp" class="col-5">
+            <img src="/images/Kj.webp" class="col-5">
+        </div>
     </div>
-    <div class="col-6">
+    <div class="col-5">
         <h2>Login</h2>
         <p>Welcome!</p>
+        <br>
         <div class="group">
             <input type="text" required>
             <span class="highlight"></span>
@@ -21,5 +26,6 @@
             <span class="bar"></span>
             <label>Password</label>
         </div>
+        <a class="button-blue-action">Sign In</a>
     </div>
 </div>
