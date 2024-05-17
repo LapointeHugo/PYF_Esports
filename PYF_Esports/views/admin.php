6521558270a4629a9a2e78f54deb8f546963e4ec
Hugo <1946091@cegepmontpetit.ca>
2024-05-16 21:38:39 -0400

    PROGRESS: panel

--- PYF_Esports/views/admin.php
+++ PYF_Esports/views/admin.php
@@ -2,27 +2,18 @@
 <!DOCTYPE html>
 <html>
     <h1 style="margin-bottom: 50px">Control Panel</h1>
+    <h2>Teams</h2>
     <div>
-        <h2>Teams</h2>
-        <div>
-            <div class="row align-items-center existingTeam" style="margin-top: 25px; margin-bottom: 25px;">
-                <img class="col-auto" src="/../images/TL.png" width="50px" height="50px">
-                <p class="col-auto" style="margin-bottom: 0px; padding-left: 0px">Team Liquid</p>
-                <p class="col-auto" style="margin-bottom: 0px;"><a style="color: rgb(0, 203, 240);" >Players</a> : Johnqt (C), Zellsis, Tenz, Zekken, Sacy</p>
-                <button class="button-blue-action col-auto" style="margin-left: 20px; margin-right: 10px">Edit</button>
-                <button class="button-red-action col-auto" style="margin-left: 10px; margin-right: 20px">Delete</button>
-                <hr style="color: grey; margin-top: 10px; width: 1000px"/>
-            </div>
-            <p>+ Create team</p>
+        <div class="row align-items-center existingTeam" style="margin-top: 25px; margin-bottom: 25px;">
+            <img class="col-auto" src="/../images/TL.png" width="50px" height="50px">
+            <p class="col-auto" style="margin-bottom: 0px; padding-left: 0px; color: rgb(0, 203, 240);">Team Liquid</p>
+            <p class="col-auto" style="margin-bottom: 0px;"><a style="color: rgb(0, 203, 240);" >Players</a> : Johnqt (C), Zellsis, Tenz, Zekken, Sacy</p>
+            <button class="button-blue-action col-auto" style="margin-left: 20px; margin-right: 10px">Edit</button>
+            <button class="button-red-action col-auto" style="margin-left: 10px; margin-right: 20px">Delete</button>
+            <hr style="color: grey; margin-top: 10px; width: 1000px"/>
         </div>
-
     </div>
-    <hr style="margin-top: 30px; color:white; margin-bottom: 50px"/>
-    <div>
-        <h2>Tournaments</h2>
-        <div>
-            <p>+ Create tournament</p>
-        </div>
+    <!-- <hr style="margin-top: 30px; color:white; margin-bottom: 50px"/> -->
+    <h2 style="margin-top: 50px">Tournaments</h2>
 
-    </div>
 </html>
