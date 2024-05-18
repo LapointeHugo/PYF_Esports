b91b1bb076c3245f1de53c399dfa324a2d42dce1
Hugo <1946091@cegepmontpetit.ca>
2024-05-18 15:43:32 -0400

    PROGRESS: Added some team logos and names

--- PYF_Esports/views/teams.php
+++ PYF_Esports/views/teams.php
@@ -3,18 +3,37 @@
 <div style="margin-top: 20px; margin-bottom: 20px">
     <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
         <div class="col-3 col-md-2 col-xl-1">
-            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
+            <img src="/images/Designer.png" alt="PYF_Logo" width="100px" height="100px" style="border-radius: 50%">
         </div>
-        <p class="teamName col-7 col-md-9 col-xl-10">ROGUE</p>
+        <p class="teamName col-7 col-md-9 col-xl-10">ValoRats</p>
     </button>
     <div class="content">
         <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
         <div class="row justify-content-around align-items-center">
-            <p class="col-12 col-md-2 roster">zekken</p>
-            <p class="col-12 col-md-2 roster">Sacy</p>
-            <p class="col-12 col-md-2 roster">TenZ</p>
-            <p class="col-12 col-md-2 roster">johnqt</p>
-            <p class="col-12 col-md-2 roster">Zellsis</p>
+            <p class="col-12 col-md-2 roster">TWYL (C)</p>
+            <p class="col-12 col-md-2 roster">Rapid</p>
+            <p class="col-12 col-md-2 roster">ouro</p>
+            <p class="col-12 col-md-2 roster">Alexx</p>
+            <p class="col-12 col-md-2 roster">Jacob</p>
+        </div>
+    </div>
+</div>
+
+<div style="margin-top: 20px; margin-bottom: 20px">
+    <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
+        <div class="col-3 col-md-2 col-xl-1">
+            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
+        </div>
+        <p class="teamName col-7 col-md-9 col-xl-10">Tristan's Lil Kittens</p>
+    </button>
+    <div class="content">
+        <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
+        <div class="row justify-content-around align-items-center">
+            <p class="col-12 col-md-2 roster">Tristan (C)</p>
+            <p class="col-12 col-md-2 roster">koffee</p>
+            <p class="col-12 col-md-2 roster">MrCleverClean</p>
+            <p class="col-12 col-md-2 roster">dong</p>
+            <p class="col-12 col-md-2 roster">Mr Peak</p>
         </div>
         <p style="margin-top: 20px">Substitutes : </p>
         <div class="row justify-content-around align-items-center" style="margin-bottom: 10px">
@@ -30,26 +49,22 @@
 <div style="margin-top: 20px; margin-bottom: 20px">
     <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
         <div class="col-3 col-md-2 col-xl-1">
-            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
+            <img src="/images/cornboyz.jpg" alt="PYF_Logo" width="100px" height="100px" style="border-radius: 50%">
         </div>
-        <p class="teamName col-7 col-md-9 col-xl-10">ROGUE</p>
+        <p class="teamName col-7 col-md-9 col-xl-10">CornBoyz</p>
     </button>
     <div class="content">
         <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
         <div class="row justify-content-around align-items-center">
-            <p class="col-12 col-md-2 roster">zekken</p>
-            <p class="col-12 col-md-2 roster">Sacy</p>
-            <p class="col-12 col-md-2 roster">TenZ</p>
-            <p class="col-12 col-md-2 roster">johnqt</p>
-            <p class="col-12 col-md-2 roster">Zellsis</p>
+            <p class="col-12 col-md-2 roster">unosam (C)</p>
+            <p class="col-12 col-md-2 roster">AwkBellz</p>
+            <p class="col-12 col-md-2 roster">inonsang</p>
+            <p class="col-12 col-md-2 roster">bonaventure</p>
+            <p class="col-12 col-md-2 roster">Mid Player</p>
         </div>
-        <p style="margin-top: 20px">Substitutes : </p>
+        <p style="margin-top: 20px">Coach : </p>
         <div class="row justify-content-around align-items-center" style="margin-bottom: 10px">
-            <p class="col-2 roster">curry</p>
-            <p class="col-2 roster">shroud</p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
+            <p class="col-2 roster">Dr. Earl MD</p>
         </div>
     </div>
 </div>
@@ -57,63 +72,9 @@
 <div style="margin-top: 20px; margin-bottom: 20px">
     <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
         <div class="col-3 col-md-2 col-xl-1">
-            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
+            <img src="/images/brimmy.jpg" alt="PYF_Logo" width="100px" height="100px" style="border-radius: 50%">
         </div>
-        <p class="teamName col-7 col-md-9 col-xl-10">ROGUE</p>
-    </button>
-    <div class="content">
-        <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
-        <div class="row justify-content-around align-items-center">
-            <p class="col-12 col-md-2 roster">zekken</p>
-            <p class="col-12 col-md-2 roster">Sacy</p>
-            <p class="col-12 col-md-2 roster">TenZ</p>
-            <p class="col-12 col-md-2 roster">johnqt</p>
-            <p class="col-12 col-md-2 roster">Zellsis</p>
-        </div>
-        <p style="margin-top: 20px">Substitutes : </p>
-        <div class="row justify-content-around align-items-center" style="margin-bottom: 10px">
-            <p class="col-2 roster">curry</p>
-            <p class="col-2 roster">shroud</p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
-        </div>
-    </div>
-</div>
-
-<div style="margin-top: 20px; margin-bottom: 20px">
-    <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
-        <div class="col-3 col-md-2 col-xl-1">
-            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
-        </div>
-        <p class="teamName col-7 col-md-9 col-xl-10">ROGUE</p>
-    </button>
-    <div class="content">
-        <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
-        <div class="row justify-content-around align-items-center">
-            <p class="col-12 col-md-2 roster">zekken</p>
-            <p class="col-12 col-md-2 roster">Sacy</p>
-            <p class="col-12 col-md-2 roster">TenZ</p>
-            <p class="col-12 col-md-2 roster">johnqt</p>
-            <p class="col-12 col-md-2 roster">Zellsis</p>
-        </div>
-        <p style="margin-top: 20px">Substitutes : </p>
-        <div class="row justify-content-around align-items-center" style="margin-bottom: 10px">
-            <p class="col-2 roster">curry</p>
-            <p class="col-2 roster">shroud</p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
-            <p class="col-2 roster"></p>
-        </div>
-    </div>
-</div>
-
-<div style="margin-top: 20px; margin-bottom: 20px">
-    <button type="button" class="collapsible row align-items-center justify-content-between" style="margin-left: 0px">
-        <div class="col-3 col-md-2 col-xl-1">
-            <img src="/images/LogoTemplate.png" alt="PYF_Logo" width="100px" height="100px">
-        </div>
-        <p class="teamName col-7 col-md-9 col-xl-10">ROGUE</p>
+        <p class="teamName col-7 col-md-9 col-xl-10">Brimmy Babies</p>
     </button>
     <div class="content">
         <p style="margin-top: 15px; margin-bottom: 15px">Main roster :</p>
